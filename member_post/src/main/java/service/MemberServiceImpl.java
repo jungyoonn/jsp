20b60615060c4e7ec28056f69ab471b7f6028f5d
jungyoonn <sophia7625@naver.com>
2024-11-04 13:11:33 +0900

    인터페이스 실제 구현

---
+++ member_post/src/main/java/service/MemberServiceImpl.java
@@ -0,0 +1,45 @@
+package service;
+
+import java.util.List;
+
+import dao.MemberDao;
+import vo.Member;
+
+public class MemberServiceImpl implements MemberService {
+	private MemberDao memberDao = MemberDao.getInstance();
+	
+	@Override
+	public int register(Member member) {
+		return memberDao.insert(member);
+	}
+
+	@Override
+	public Member findBy(String id) {
+		
+		return null;
+	}
+
+	@Override
+	public boolean login(String id, String pw) {
+		
+		return false;
+	}
+
+	@Override
+	public List<Member> list() {
+		
+		return null;
+	}
+
+	@Override
+	public boolean remove(String id) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean modify(Member member) {
+		
+		return false;
+	}
+}
