236d98919d4b65c9d4b3c51095708e979891a404
jungyoonn <sophia7625@naver.com>
2024-11-04 17:48:19 +0900

    로그인

---
+++ member_post/src/main/java/servlet/member/Signin.java
@@ -0,0 +1,42 @@
+package servlet.member;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.MemberService;
+import service.MemberServiceImpl;
+import vo.Member;
+
+@WebServlet("/signin")
+public class Signin extends HttpServlet{
+	private MemberService service = new MemberServiceImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/jsp/member/signin.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		String id = req.getParameter("id");
+		String pwd = req.getParameter("pwd");
+		
+		System.out.println(id + " :::: " + pwd);
+		
+		if(service.login(id, pwd)) {
+			// 로그인 성공
+			resp.sendRedirect("signin");
+		} else {
+			// 로그인 실패
+			resp.sendRedirect("signup");
+		}
+		
+	}
+}
