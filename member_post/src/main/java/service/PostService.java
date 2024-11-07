67eebd2a05751e45621631fba35065650793353e
jungyoonn <sophia7625@naver.com>
2024-11-07 14:46:15 +0900

    게시판 인터페이스

---
+++ member_post/src/main/java/service/PostService.java
@@ -0,0 +1,17 @@
+package service;
+
+import java.util.List;
+
+import vo.Post;
+
+public interface PostService {
+	int write(Post post);
+	
+	int modify(Post post);
+	
+	int remove(Long pno);
+	
+	Post findBy(Long pno);
+	
+	List<Post> list();
+}
