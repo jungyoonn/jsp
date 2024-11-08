c430ad029111b63bd59848030dfdf84fb76a3ab5
jungyoonn <sophia7625@naver.com>
2024-11-08 14:49:33 +0900

    디비 연결 정보 수정

--- member_post/src/main/java/utils/DBConn.java
+++ member_post/src/main/java/utils/DBConn.java
@@ -1,12 +1,26 @@
 package utils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConn {
 	public static Connection getConnection() throws ClassNotFoundException, SQLException{
-		Class.forName("org.mariadb.jdbc.Driver");
-		return DriverManager.getConnection("jdbc:mariadb://43.203.219.241:3306/post", "sample", "1234");
+		Properties props = new Properties();
+		InputStream is = DBConn.class.getClassLoader().getResourceAsStream("db.properties");
+		try {
+			props.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String driver = props.getProperty("driver");
+		String host = props.getProperty("host");
+		String username = props.getProperty("username");
+		String password = props.getProperty("password");
+		Class.forName(driver);
+		return DriverManager.getConnection(host, username, password);
 	}
 }
