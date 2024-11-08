3cc5ba7858a7c72a47bd643d0fb8bab25b4458db
jungyoonn <sophia7625@naver.com>
2024-11-08 16:49:30 +0900

    글 수정 서블릿 구성

---
+++ member_post/src/main/java/servlet/post/PostUpdate.java
@@ -0,0 +1,69 @@
+package servlet.post;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.PostService;
+import service.PostServiceImpl;
+import utils.Commons;
+import vo.Member;
+import vo.Post;
+
+@WebServlet("/post/modify")
+public class PostUpdate extends HttpServlet{
+	private PostService service = new PostServiceImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String pnoStr = req.getParameter("pno");
+		Object memberObj = req.getSession().getAttribute("member");
+		
+		if(pnoStr == null || memberObj == null) {
+			Commons.printMsg("비정상적인 접근입니다", "list", resp);
+			return;
+		}
+		
+		Long pno = Long.valueOf(pnoStr);
+		Member m = (Member) memberObj;
+		
+		if(!m.getId().equals(service.findBy(pno).getWriter())) {
+			Commons.printMsg("본인이 작성한 글만 수정할 수 있습니다", "list" + pno, resp);
+			return;
+		}
+		
+		req.setAttribute("post", service.findBy(pno));
+		req.getRequestDispatcher("/WEB-INF/jsp/post/modify.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Object memberObj = req.getSession().getAttribute("member");
+		
+		if(memberObj == null) {
+			Commons.printMsg("비정상적인 접근입니다", "list", resp);
+			return;
+		}
+		
+		Member m = (Member) memberObj;
+		
+		Long pno = Long.valueOf(req.getParameter("pno"));
+		String title = req.getParameter("title");
+		String content = req.getParameter("content");
+		
+		if(!m.getId().equals(service.findBy(pno).getWriter())) {
+			Commons.printMsg("본인이 작성한 글만 수정할 수 있습니다", "view?pno=" + pno, resp);
+			return;
+		}
+		service.modify(Post.builder()
+				.pno(pno)
+				.title(title)
+				.content(content)
+				.build());
+		resp.sendRedirect("view?pno=" + pno);
+	}
+	
+}
