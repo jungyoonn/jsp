f83dca57099672ad74155aa4cfa35941981389ef
jungyoonn <sophia7625@naver.com>
2024-11-12 12:46:20 +0900

    서비스 구현부 마이바티스로 교체

--- member_post/src/main/java/service/PostServiceImpl.java
+++ member_post/src/main/java/service/PostServiceImpl.java
@@ -2,49 +2,70 @@ package service;
 
 import java.util.List;
 
-import dao.PostDao;
+import org.apache.ibatis.session.SqlSession;
+
 import dto.Criteria;
+import mapper.PostMapper;
+import utils.MybatisInit;
 import vo.Post;
 
 public class PostServiceImpl implements PostService {
-	private PostDao postDao = new PostDao();
 	
 	@Override
 	public int write(Post post) {
-		return postDao.insert(post);
+		try(SqlSession session = MybatisInit.getInstance().sqSessionFactory().openSession(true)) {
+			PostMapper mapper = session.getMapper(PostMapper.class);
+			return mapper.insert(post);
+		}
 	}
 
 	@Override
 	public int modify(Post post) {
-		return postDao.update(post);
+		try(SqlSession session = MybatisInit.getInstance().sqSessionFactory().openSession(true)) {
+			PostMapper mapper = session.getMapper(PostMapper.class);
+			return mapper.update(post);
+		}
 	}
 
 	@Override
 	public int remove(Long pno) {
-		return postDao.delete(pno);
+		try(SqlSession session = MybatisInit.getInstance().sqSessionFactory().openSession(true)) {
+			PostMapper mapper = session.getMapper(PostMapper.class);
+			return mapper.delete(pno);
+		}
 	}
 
 	@Override
 	public Post findBy(Long pno) {
-		Post p = postDao.selectOne(pno);
-//		return p != null ? postDao.selectOne(pno) : null;
-		return p;
+		try(SqlSession session = MybatisInit.getInstance().sqSessionFactory().openSession(true)) {
+			PostMapper mapper = session.getMapper(PostMapper.class);
+			return mapper.selectOne(pno);
+		}
 	}
 
 	@Override
 	public List<Post> list(Criteria cri) {
-		return postDao.selectList(cri);
+		try(SqlSession session = MybatisInit.getInstance().sqSessionFactory().openSession(true)) {
+			PostMapper mapper = session.getMapper(PostMapper.class);
+			return mapper.selectList(cri);
+		}
 	}
 
 	@Override
 	public Post view(Long pno) {
-		postDao.increaseViewCount(pno);
+		try(SqlSession session = MybatisInit.getInstance().sqSessionFactory().openSession(true)) {
+			PostMapper mapper = session.getMapper(PostMapper.class);
+			mapper.increaseViewCount(pno);
+		}
 		return findBy(pno);
 	}
 
 	@Override
 	public int count(Criteria cri) {
-		return postDao.getCount(cri);
+		try(SqlSession session = MybatisInit.getInstance().sqSessionFactory().openSession(true)) {
+			PostMapper mapper = session.getMapper(PostMapper.class);
+			return mapper.getCount(cri);
+		}
 	}
 	
 	
