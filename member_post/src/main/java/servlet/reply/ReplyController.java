e3de2fe79e94a4ea7fb6a49bfc2adbc135e92465
jungyoonn <sophia7625@naver.com>
2024-11-15 18:20:32 +0900

    댓글 컨트롤러 수정

--- member_post/src/main/java/servlet/reply/ReplyController.java
+++ member_post/src/main/java/servlet/reply/ReplyController.java
@@ -8,19 +8,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.codehaus.jackson.map.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import service.ReplyService;
 import service.ReplyServiceImpl;
+import vo.Reply;
 
 @WebServlet("/reply/*")
 public class ReplyController extends HttpServlet{
 	private ReplyService service = ReplyServiceImpl.getInstance();
+	private Gson gson = new GsonBuilder().setDateFormat("yyyy/MM/dd HH:mm:ss").create();
 	
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doDelete(req, resp);
+		String uri = req.getRequestURI();
+		uri = uri.replace(req.getContextPath() + "/reply/", "");
+		
+		if(uri.startsWith("list")) { // 목록 조회
+			int tmpIdx = uri.lastIndexOf("/");
+			Long pno = 0L;
+			if(tmpIdx != -1) {
+				pno = Long.valueOf(uri.substring(tmpIdx + 1));
+			}
+			service.removeAll(pno);
+		} else { // 단일 조회
+			Long rno = Long.valueOf(uri);
+			service.remove(rno);
+		}
 	}
 
 	@Override
@@ -41,22 +56,28 @@ public class ReplyController extends HttpServlet{
 			ret = service.findBy(rno);
 		}
 		resp.setContentType("application/json; charset=utf-8");
-		resp.getWriter().print(new ObjectMapper().writeValueAsString(ret));
+		resp.getWriter().print(gson.toJson(ret));
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("replyController.doPost()");
+		Reply reply = gson.fromJson(req.getReader(), Reply.class);
+		System.out.println(reply);
+		service.write(reply);
+	}
+
+	@Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		char[] chs = new char[req.getContentLength()];
 		req.getReader().read(chs);
 		String str = new String(chs);
 		System.out.println(str);
-	}
+		
+		Reply reply = gson.fromJson(str, Reply.class);
+		System.out.println(reply);
+		
+		service.modify(reply);
 
-	@Override
-	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPut(req, resp);
-	}
-	
+    }
+
 }
