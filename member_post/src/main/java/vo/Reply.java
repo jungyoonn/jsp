dbfd2fb87768bdf9e82b2618ef3f33676ba02740
jungyoonn <sophia7625@naver.com>
2024-11-15 15:56:37 +0900

    미사용 메서드 삭제

--- member_post/src/main/java/vo/Reply.java
+++ member_post/src/main/java/vo/Reply.java
@@ -2,11 +2,15 @@ package vo;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Reply {
 	private Long rno;
 	private String content;
