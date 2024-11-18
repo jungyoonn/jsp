44f284799bca1c12b4920dc60b3ed05f52bc0dd5
jungyoonn <sophia7625@naver.com>
2024-11-18 12:49:17 +0900

    reply.js

--- member_post/src/main/webapp/js/reply.js
+++ member_post/src/main/webapp/js/reply.js
@@ -4,12 +4,37 @@ const replyService = (function() {
 
     function write(reply, callback) {
         console.log(reply);
-		if(callback) callback();
+        const data = JSON.stringify(reply);
+        $.post({url, data})
+        .done(function(data) {
+			if(callback) callback(data);
+        })
     }
     function list(pno, callback) {
         $.getJSON(url + "/list/" + pno).done(function(data) {
             if(callback) callback(data);
         });
     }
-    return {write, list}
+    function view(rno, callback) {
+        $.getJSON(url + "/" + rno).done(function(data) {
+            if(callback) callback(data);
+        })
+    }
+    function modify(reply, callback) {
+        const data = JSON.stringify(reply);
+        $.ajax(url, {
+            method : 'put',
+            data
+        }).done(function(data) {
+            if(callback) callback(data);
+        })
+    }
+    function remove(rno, callback) {
+        $.ajax(url + "/" + rno, {
+            method : 'delete'
+        }).done(function(data) {
+            if(callback) callback(data);
+        })
+    }
+    return {write, list, view, modify, remove}
 })();
